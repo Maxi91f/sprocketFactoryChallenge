7391b4408f7d1b47c8e1404db38611ab2d7b4162
Maxi <45922974+Maxi91f@users.noreply.github.com>
2023-03-29 01:31:29 -0300

    Added postgresql and docker-compose

--- sprocket_factory/config.py
+++ sprocket_factory/config.py
@@ -2,9 +2,13 @@
 Configuration for the Flask app
 """
 # pylint: disable=too-few-public-methods
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     """
     Configuration for the Flask app
     """
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sprocket.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
